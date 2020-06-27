21751fb8b1deb5fde8a962e9fbad541fd73cbd42
Vyacheslav <55537294+AkanoCA@users.noreply.github.com>
2020-06-27 19:20:22 +0300

    Add files via upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,23 @@
 (function () {
 	"use strict";
 
+	function isDash() {
+		if (document.querySelector('.literature__dash').checked) {
+			return '&#8212';
+		} else {
+			return '';
+		}
+	}
+
+	function normalize(block) {
+		let inputs = block.querySelectorAll('input');
+
+		inputs.forEach(function (item) {
+			item.value = '';
+		});
+		block.querySelector('.side__checkbox').checked = false;
+	}
+
 	function createAuthors(arr) { //функция создания списка авторов с фамилией и инициалами
 		function Separate(name) { //формирует объект с фамилией и инициалами
 			let arr = name.split(' ');
@@ -19,9 +36,8 @@
 		return authors;
 	}
 
-	function checkCity (city) {
-		let cityArr = [
-			{
+	function checkCity(city) {
+		let cityArr = [{
 				name: "москва",
 				abb: "М.",
 			},
@@ -33,7 +49,7 @@
 
 		let check = city.toLowerCase();
 
-		cityArr.forEach(function(item){
+		cityArr.forEach(function (item) {
 			console.log(city);
 			if (item.name == check) {
 				city = item.abb;
@@ -49,57 +65,72 @@
 
 
 
+
+
 		bookButton.addEventListener('click', function (e) {
 			e.preventDefault();
 
+			let dash = isDash();
+
 			let arrAuthors = book.querySelector('.js_name').value;
+			if (!arrAuthors) {
+				return alert("Введите хотя-бы одного автора");
+			}
 
 			let authors = createAuthors(arrAuthors);
 
 			let title = book.querySelector('.js_title').value;
+			if (!title) {
+				return alert("Введите название книги");
+			}
+
 			let city = book.querySelector('.js_city').value;
+			if (!city) {
+				return alert("Введите город издателя");
+			}
 			city = checkCity(city);
 
 
 			let publisher = book.querySelector('.js_publisher').value;
+			if (!publisher) {
+				return alert("Введите название издателя");
+			}
+
 			let year = book.querySelector('.js_year').value;
+			if (!year) {
+				return alert("Введите год издания");
+			}
+
 			let pages = book.querySelector('.js_pages').value;
+			if (!pages) {
+				return alert("Введите количество страниц в книге");
+			}
+
 			let bookCheck = book.querySelector('.side__checkbox').checked;
 			let reference = "";
 
 
 			if (bookCheck && authors.length >= 3) {
-				reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}, ${authors[2].initials} ${authors[2].surname} [и др.]. &#8212 ${city}: ${publisher}, ${year} . &#8212 ${pages} с.`;
+				reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}, ${authors[2].initials} ${authors[2].surname} [и др.]. ${dash} ${city}: ${publisher}, ${year} . ${dash} ${pages} с.`;
 			} else if (!bookCheck && authors.length <= 3) {
 				switch (authors.length) {
 					case 1:
-						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}. &#8212 ${city}: ${publisher}, ${year}. &#8212 ${pages} с.`;
+						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}. ${dash} ${city}: ${publisher}, ${year}. ${dash} ${pages} с.`;
 						break;
 					case 2:
-						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}. &#8212 ${city}: ${publisher}, ${year}. &#8212 ${pages} с.`;
+						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}. ${dash} ${city}: ${publisher}, ${year}. ${dash} ${pages} с.`;
 						break;
 					case 3:
-						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}, ${authors[2].initials} ${authors[2].surname}. &#8212 ${city}: ${publisher}, ${year}. &#8212 ${pages} с.`;
+						reference = `${authors[0].surname}, ${authors[0].initials} ${title} / ${authors[0].initials} ${authors[0].surname}, ${authors[1].initials} ${authors[1].surname}, ${authors[2].initials} ${authors[2].surname}. ${dash} ${city}: ${publisher}, ${year}. ${dash} ${pages} с.`;
 						break;
 				}
 			} else {
 				return alert("Необходимо написать первых 3х авторов");
 			}
 
-
-			
-
-			
-
 			arrBook.push(reference);
 			arrBook.sort();
-			
-			let inputs = book.querySelectorAll('input');
-
-			inputs.forEach(function(item){
-				item.value = '';
-			});
-			book.querySelector('.side__checkbox').checked = false;
+			normalize(book);
 
 			renderList();
 		});
